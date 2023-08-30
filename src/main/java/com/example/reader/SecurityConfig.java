53c29114bc37d4b9a3f54001e816e3f018b36c4c
mhermkrtchyan777 <mher.mkrtchyan2000@gmail.com>
2023-08-30 12:10:46 +0400

    Added some Tests

--- src/main/java/com/example/reader/SecurityConfig.java
+++ src/main/java/com/example/reader/SecurityConfig.java
@@ -5,13 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-<<<<<<< HEAD
-import org.springframework.security.core.userdetails.UserDetailsService;
-=======
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
->>>>>>> c4d4d83 (Added some Tests)
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,26 +21,11 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-<<<<<<< HEAD
-        http.authorizeHttpRequests(authorize-> authorize.requestMatchers("/").hasRole("READER")).
-                formLogin(log->log.loginPage("/login").failureUrl("/login?error=true"));
-        return http.build();
-    }
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return id -> repository.getReferenceById(id);
-    }
-}
-=======
+
         http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/").hasRole("READER")).
                 formLogin(log -> log.loginPage("/login").failureUrl("/login?error=true"));
         return http.build();
     }
-
-    /*@Bean
-    public UserDetailsService userDetailsService() {
-        return id -> repository.getReferenceById(id);
-    }*/
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -63,4 +43,4 @@ public class SecurityConfig {
     }
 }
 
->>>>>>> c4d4d83 (Added some Tests)
+
